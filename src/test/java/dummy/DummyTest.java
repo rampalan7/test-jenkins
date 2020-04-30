e53e213d7e5de5b71fe905a62cf790ee64f25b30
rampalan <ramasamy.palanivel@hotmail.com>
2020-04-30 22:16:24 +0100

    first commit

---
+++ src/test/java/dummy/DummyTest.java
@@ -0,0 +1,14 @@
+package dummy;
+
+import jdk.internal.jline.internal.TestAccessible;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    public void testOne(){
+        assertEquals(1,1);
+    }
+}
